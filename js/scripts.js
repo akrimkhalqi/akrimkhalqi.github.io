f39b8a48a35ec3a802cb327a3c01fcd47ff61e00
unknown <akrimkhalqi11@gmail.com>
2020-08-06 02:47:44 +0800

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -34,7 +34,7 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     // Load page content
-    const page = window.location.hash.substr(1);
+    var page = window.location.hash.substr(1);
     if (page == '') page = 'home';
     loadPage(page);
 
