5a3c5ecee5accc9c9e18dc1468ad5c1988a1a868
unknown <akrimkhalqi11@gmail.com>
2020-08-06 02:43:14 +0800

    initial commit

--- service-worker.js
+++ service-worker.js
@@ -88,6 +88,20 @@ if (workbox) {
         ignoreUrlParametersMatching: [/.*/]
     });
 
+    // Routing workbox untuk menyimpan cache images
+    workbox.routing.registerRoute(
+        /\.(?:png|gif|jpg|jpeg|svg)$/,
+        workbox.strategies.cacheFirst({
+            cacheName: 'images',
+            plugins: [
+                new workbox.expiration.Plugin({
+                    maxEntries: 60,
+                    maxAgeSeconds: 30 * 24 * 60 * 60, // 30 hari
+                }),
+            ],
+        }),
+    );
+
     // Routing workbox untuk menyimpan cache dari data api.football-data.org
     workbox.routing.registerRoute(
         new RegExp("https://api.football-data.org/v2/"),
